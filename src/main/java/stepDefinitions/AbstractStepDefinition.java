7390cf4cd14839e04ab6f1697e3f6d8c288759e9
adeekoadekoya <adeekoadekoya@gmail.com>
2021-09-30 11:58:05 -0700

    changes the path to chromedriver

--- src/main/java/stepDefinitions/AbstractStepDefinition.java
+++ src/main/java/stepDefinitions/AbstractStepDefinition.java
@@ -1,4 +1,6 @@
 package stepDefinitions;
+import java.util.Properties;
+
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,14 +9,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AbstractStepDefinition {
 
-	
+	Properties properties = new Properties();
 	private static WebDriver driver = null;
 	private static Logger log = Logger.getLogger(AbstractStepDefinition.class);
 	
 	public WebDriver getDriver(){
 		 
-   	 System.setProperty("webdriver.chrome.driver", "//Users//adekoyaadeeko//Downloads//chromedriver2");
-		
+   	 System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//src//main//resources//chromedriver2"); 
+   
    	 if(driver == null) {
    		log.info("Initializing step1 chrome driver");
 			driver = new ChromeDriver();
